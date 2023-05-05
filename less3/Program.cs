0ac8119115be9f74a35562d3fa31ead52167848d
Roman <poma710@yandex.run>
2023-05-05 19:19:03 +0300

    DONE

---
+++ less3/Program.cs
@@ -0,0 +1,15 @@
+﻿int[,] array = new int[,] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+int rows = array.GetLength(0);
+int cols = array.GetLength(1);
+
+
+for (int j = 0; j < cols; j++)
+{
+    int sum = 0;
+    for (int i = 0; i < rows; i++)
+    {
+        sum += array[i, j];
+    }
+    double avg = (double)sum / rows;
+    Console.WriteLine("Среднее арифметическое элементов в столбце {0} равно {1}.", j, avg);
+}
